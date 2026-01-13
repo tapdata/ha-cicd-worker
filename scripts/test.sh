eb10bcbd0d31b7a982994e13525b7959e134a6ff
sam.p <102567930@qq.com>
2026-01-13 11:56:22 +0800

    ci: support manual workflow with environment parameter
    - Switch workflow trigger to workflow_dispatch
    - Add env input (dev / lpt / prod) for manual runs
    - Pass env as argument to script/test.sh
    - Log selected environment in /tmp/github_actions_run.log on runner

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,6 +8,8 @@ USER_NAME="$(whoami)"
 RUN_ID="${GITHUB_RUN_ID:-unknown}"
 REPO="${GITHUB_REPOSITORY:-unknown}"
 WORKFLOW="${GITHUB_WORKFLOW:-unknown}"
-JOB="${GITHUB_JOB:-unknown}"
 
-echo "[$NOW] host=$HOST user=$USER_NAME repo=$REPO workflow=$WORKFLOW job=$JOB run_id=$RUN_ID" >> "$LOG_FILE"
+JOB="${GITHUB_JOB:-unknown}"
+ENV_NAME="${1:-unknown}"
+
+echo "[$NOW] host=$HOST user=$USER_NAME repo=$REPO workflow=$WORKFLOW job=$JOB env=$ENV_NAME run_id=$RUN_ID" >> "$LOG_FILE"
